087d39a5c12836be9b2c7abca5fa3b7a01a5444f
aaronjames0 <aaronjames0@yahoo.com>
2024-10-10 19:06:05 -0700

    Daily Commit

--- Python3/1942Medium.py
+++ Python3/1942Medium.py
@@ -2,15 +2,15 @@ class Solution:
     def smallestChair(self, times: List[List[int]], targetFriend: int) -> int:
         seats = []
         open_seat = 0
-        deapartures = []
+        departures = []
         max_time = times[targetFriend][0]
         arrivals = [(arrive, leave) for i, (arrive, leave) in enumerate(times) if arrive < max_time]
         heapify(arrivals)
 
         while arrivals:
             arrive, leave = heappop(arrivals)
-            while deapartures and deapartures[0][0] <= arrive:
-                time, seat = heappop(deapartures)
+            while departures and departures[0][0] <= arrive:
+                time, seat = heappop(departures)
                 if seat == open_seat - 1:
                     open_seat -= 1
                 else:
@@ -21,14 +21,14 @@ class Solution:
                 seat = open_seat
                 open_seat += 1
             if leave <= max_time:
-                heappush(deapartures, (leave, seat))
+                heappush(departures, (leave, seat))
             
         if seats:
             result = seats[0]
         else:
             result = open_seat
             
-        for time, seat in deapartures:
+        for time, seat in departures:
             if seat < result:
                 result = seat
         return result
